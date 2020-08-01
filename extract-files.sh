50e28f10ab0d1b95ee76291fe855e212b5b4f837
ZeeLog <Danjel.Zammit@gmail.com>
2020-08-01 10:39:25 +0200

    mido: Restore previous wifi implementation
    This is a squashed update of the following commits:
    
    Revert "mido: Stop copying WCNSS_qcom_cfg.ini to userdata"
    This reverts commit 9b47c33e778488b65ce32a7434166ff582a6283a.
    
    Revert "mido: wifi: build from vendor tree"
    This reverts commit ce432281cf1f8ea7a149e1a4f87cdad6a36386eb.
    
    mido: sepolicy: address  wifi denials
    Change-Id: Ic77f3305178ab7c3f158804742f7ee23416363c7
    
    Revert "mido: wifi: Update and modify wcnss_service."
    This reverts commit 38dedadf54f2c193b0c8b4662b80c3c2c3dbdd87.
    
    Revert "mido: Provide module targets for libqmiservices"
    This reverts commit 4b88b9f3dcdc4d7c2a70c078d7f4abd12715c32a.
    
    * Also restore wcnss_service from Tissot
    
    Change-Id: I4be8acf85a21dab707fd3cc390abaf5c8e8cfd2b

--- extract-files.sh
+++ extract-files.sh
@@ -79,6 +79,7 @@ done
 # Camera debug log file
 sed -i "s|persist.camera.debug.logfile|persist.vendor.camera.dbglog|g" "${DEVICE_BLOB_ROOT}"/vendor/lib/libmmcamera_dbg.so
 "${MY_DIR}/setup-makefiles.sh"
+<<<<<<< HEAD
 
 # Camera graphicbuffer shim
 patchelf --add-needed libui_shim.so  "${DEVICE_BLOB_ROOT}"/vendor/lib/libmmcamera_ppeiscore.so
@@ -107,3 +108,5 @@ patchelf --add-needed "libshim_wfdmmsink.so" "${DEVICE_BLOB_ROOT}"/lib/libwfdmms
 # Wcnss_service - libqmiservices_shim
 patchelf --add-needed "libqmiservices_shim.so" "${DEVICE_BLOB_ROOT}"/vendor/bin/wcnss_service
 sed -i "s|dms_get_service_object_internal_v01|dms_get_service_object_shimshim_v01|g" "${DEVICE_BLOB_ROOT}"/vendor/bin/wcnss_service
+=======
+>>>>>>> d0f58116... mido: Restore previous wifi implementation
